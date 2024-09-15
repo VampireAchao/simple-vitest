c5a6225a3b538bf75562b7797391c92920305d98
VampireAchao <achao@apache.org>
2024-09-15 13:47:08 +0800

    test wrong ci

--- test/sum.test.ts
+++ test/sum.test.ts
@@ -5,3 +5,7 @@ import {sum} from "../src/sum.ts"
 test('adds 1 + 2 to equal 3', () => {
     expect(sum(1, 2)).toBe(3)
 })
+
+test('adds 1 + 1 to equal 3', () => {
+    expect(sum(1, 1)).toBe(3)
+})
